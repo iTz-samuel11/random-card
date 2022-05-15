d00c34bae63b2cb1244ca5ce5827724ffd59a35f
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-05-15 04:43:32 +0000

    avance de la carta

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let numero = document.querySelector("#numero");
+let pinta1 = document.querySelector("#pinta-1");
+let pinta2 = document.querySelector("#pinta-2");
+let numeros = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+numero.innerHTML = numeros[Math.floor(Math.random() * 13)];
+
+let pintas = ["♦", "♥", "♣", "♠"];
+let pinta = pintas[Math.floor(Math.random() * 4)];
+pinta1.innerHTML = pinta;
+pinta2.innerHTML = pinta;
+
+console.log(numero);
